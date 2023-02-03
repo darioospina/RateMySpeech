6dd12b351260b34d93eff99d503fb8a6d6897f0e
pat84571 <pat84571@gmail.com>
2023-02-02 20:27:58 -0700

    Created list of events with hardcoded data. Centering inside box is off but can be figured out later

--- client/src/Components/UpcomingEvents.js
+++ client/src/Components/UpcomingEvents.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import ListGroup from 'react-bootstrap/ListGroup';
+
+{/*NEEDS TO BE FINISHED WITH DB. List of events must be connected with database still */}
+
 export const UpcomingEvents = () => {
     return (
         <div>
-        <p>Upcoming Events</p>
+            <h1>Upcoming Events In the Next Two Weeks</h1>
+            <div style={{border:'solid black', width:'max-content'}}>
+            <ListGroup as="ol">
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold', marginRight:'10px'}} href='/SingleEvent'>Birds</a>
+                    April 2, 2023
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold', marginRight:'10px'}} href='/SingleEvent'>Trees</a>
+                    March 3, 2023
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold', marginRight:'10px'}} href='/SingleEvent'>Event Title</a>
+                    Date
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold', marginRight:'10px'}} href='/AllEvents'> ~ All Events ~</a>
+                </ListGroup.Item>
+            </ListGroup>
+            </div>
         </div>    
     )
 }
