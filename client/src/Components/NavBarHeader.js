7dce35efc7de5d6b7b4711d14b83431a7623ffd5
pat84571 <pat84571@gmail.com>
2023-02-03 15:37:10 -0700

    Changing Name and Maybe file name

--- client/src/Components/NavBarHeader.js
+++ client/src/Components/NavBarHeader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Logo from '../Images/logo.png'
 
-export const Header2 = () => {
+export const NavBar = () => {
     return (
         <div className='divHeader'>
         <img src={Logo} className="header2-logo" alt="logo"/>
