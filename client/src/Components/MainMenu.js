ec24fa2945f828320adc75907645751625f1c928
pat84571 <pat84571@gmail.com>
2023-02-02 21:21:16 -0700

    MainMenu Comp Created

--- client/src/Components/MainMenu.js
+++ client/src/Components/MainMenu.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+import ListGroup from 'react-bootstrap/ListGroup';
+
 export const MainMenu = () => {
     return (
         <div>
-        <p>Main Menu</p>
+        <h1>Main Menu</h1>
+            <div style={{border:'solid black', width:'max-content', paddingRight:"20px"}}>
+            <ListGroup as="ol">
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold'}} href='/Dashboard'>
+                        Dashboard</a>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold'}} href='/SingleEvent'>
+                        Update My Profile</a>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold'}} href='/Report'>
+                        My Scores
+                    </a>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <a style={{fontWeight:'bold'}} href='/AllEvents'>
+                        Create a New Event
+                    </a>
+                </ListGroup.Item>
+            </ListGroup>
+            </div>
         </div>    
     )
 }
