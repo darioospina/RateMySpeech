e81b805ab19898c111b7e721388bf9ca0467d79e
darioospina <65924368+darioospina@users.noreply.github.com>
2023-02-02 20:46:12 -0700

    Amended some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import './Styles/App.css';
 // Components
 import {Header1} from './Components/Header1'
 import {Footer} from './Components/Footer'
+import {ProfileDetails} from './Components/ProfileDetails'
+import {ProfileEdit} from './Components/ProfileEdit'
 
 // Pages
 import {Login} from './Pages/Login'
@@ -16,6 +18,10 @@ function App() {
       {/* {window.location.pathname === "/" ? null : <Header1/>} */}
       <Routes>
         <Route path="/" index element={<Login />}/>
+
+        
+        <Route path="/profiledetails" element={<ProfileDetails />}/>
+        <Route path="/profileedit" element={<ProfileEdit />}/>
       </Routes>
       <Footer/>
     </Router>
