46c374ec8e8b50b89382cdcb338fb1eabc26ae04
pat84571 <pat84571@gmail.com>
2023-02-08 16:31:44 -0700

    Updated ListOfEvents to match WireFrame

--- client/src/Components/ListOfEvents.js
+++ client/src/Components/ListOfEvents.js
@@ -1,31 +1,55 @@
 import React from 'react'
+
+//Import Table and Navigation properties
 import Table from 'react-bootstrap/Table'
+import NavLink from 'react-bootstrap/esm/NavLink'
+
+// Import Icons
+import {FaSearchPlus} from 'react-icons/fa'
+import {AiFillDelete} from 'react-icons/ai'
 
 export const ListOfEvents = () => {
     return (
       <div>
-      <h1>List Of All Events</h1>
+      <h1>List Of Your Events</h1>
     <Table striped>
       <thead>
         <tr>
           <th>Speaker</th>
-          <th>Presentation Topic</th>
+          <th>Event Name</th>
+          <th>Expected Audience Size</th>
           <th>Location</th>
-          <th>Date</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>Mark Otto</td>
           <td>Birds</td>
+          <td>250</td>
           <td>Calgary, AB</td>
-          <td>April 2, 2023</td>
+          <td>
+          <NavLink className='AllEvents-Actions'>
+              <FaSearchPlus />
+            </NavLink> 
+            <NavLink className='AllEvents-Actions'>
+              <AiFillDelete className='delete' />
+            </NavLink>
+          </td>
         </tr>
         <tr>
           <td>John Johnson</td>
           <td>Trees</td>
+          <td>500</td>
           <td>Austim, TX</td>
-          <td>March 3, 2023</td>
+          <td>
+            <NavLink className='AllEvents-Actions'>
+              <FaSearchPlus />
+            </NavLink> 
+            <NavLink className='AllEvents-Actions'>
+              <AiFillDelete className='delete' />
+            </NavLink>
+          </td>
         </tr>
       </tbody>
     </Table>  
