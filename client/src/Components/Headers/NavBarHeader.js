2a3a79b16a1fc7d0f3f021278e16a088a4ce023e
pat84571 <pat84571@gmail.com>
2023-02-07 20:32:50 -0700

    created NavBar and fixed image issue

--- client/src/Components/Headers/NavBarHeader.js
+++ client/src/Components/Headers/NavBarHeader.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import Logo from '../Images/logo.png'
+import '../../Styles/PatStyles.css'
+import Logo from '../../Images/logo.png'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {CgProfile} from 'react-icons/cg'
+import {RiDashboardLine} from 'react-icons/ri'
 
 export const NavBarHeader = () => {
     return (
         <div className='divHeader'>
-        <img src={Logo} className="header2-logo" alt="logo"/>
-        </div>    
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/Dashboard"><img src={Logo} className="header2-logo" alt="logo" /></Navbar.Brand>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/Dashboard">Dashboard <RiDashboardLine/> </Nav.Link>
+                            <Nav.Link href="/profiledetails">My Profile <CgProfile/> </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     )
 }
