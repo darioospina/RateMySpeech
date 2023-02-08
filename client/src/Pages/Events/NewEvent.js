a796a52f7f2715561e01f6fa598896ec490a6fd9
pat84571 <pat84571@gmail.com>
2023-02-07 20:35:21 -0700

    Added Navbar to pages i think needs it

--- client/src/Pages/Events/NewEvent.js
+++ client/src/Pages/Events/NewEvent.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { NavBarHeader } from '../../Components/Headers/NavBarHeader'
 
 export const NewEvent = () => {
     return (
         <div className='divContainer'>
+            <NavBarHeader/>
         <p>New Event</p>
         </div>    
     )
--- client/src/Pages/Events/SingleEvent.js
+++ client/src/Pages/Events/SingleEvent.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { EventDetails } from '../../Components/EventDetails'
-
+import { NavBarHeader } from '../../Components/Headers/NavBarHeader'
 
 export const SingleEvent = () => {
     return (
         <div className='divContainer'>
+            <NavBarHeader/>
         <p>Single Event</p>
         <EventDetails />
 
