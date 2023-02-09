c8d1e76be2da867fa448469ad3aba4777c111404
pat84571 <pat84571@gmail.com>
2023-02-09 16:10:09 -0700

    created Dummy Data folder and file, more to come

---
+++ client/src/Dummy Data/Events.js
@@ -0,0 +1,16 @@
+Events = [
+    {
+        speaker: "Mark Otto",
+        eventName: "Birds",
+        audienceSize: "250",
+        Location: "Calgary, AB"
+    },
+    {
+        speaker: "John Johnson",
+        eventName: "Trees",
+        audienceSize: "500",
+        Location: "Austin, TX"
+    }
+]
+
+export default Events;
