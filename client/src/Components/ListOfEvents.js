1eef6e80d189d57be7aabc1a3c024296eab13c3c
pat84571 <pat84571@gmail.com>
2023-02-09 17:04:54 -0700

    pulls dynamically from dummy data

--- client/src/Components/ListOfEvents.js
+++ client/src/Components/ListOfEvents.js
@@ -1,58 +1,53 @@
 import React from 'react'
 
+//Import Dummy Data
+import EventsData from '../Dummy Data/Events'
+
 //Import Table and Navigation properties
 import Table from 'react-bootstrap/Table'
 import NavLink from 'react-bootstrap/esm/NavLink'
 
 // Import Icons
-import {FaSearchPlus} from 'react-icons/fa'
-import {AiFillDelete} from 'react-icons/ai'
+import { FaSearchPlus } from 'react-icons/fa'
+import { AiFillDelete } from 'react-icons/ai'
 
 export const ListOfEvents = () => {
-    return (
-      <div>
-      <h1>List Of Your Events</h1>
-    <Table striped>
-      <thead>
-        <tr>
-          <th>Speaker</th>
-          <th>Event Name</th>
-          <th>Expected Audience Size</th>
-          <th>Location</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Mark Otto</td>
-          <td>Birds</td>
-          <td>250</td>
-          <td>Calgary, AB</td>
-          <td>
-          <NavLink className='AllEvents-Actions'>
-              <FaSearchPlus />
-            </NavLink> 
-            <NavLink className='AllEvents-Actions'>
-              <AiFillDelete className='delete' />
-            </NavLink>
-          </td>
-        </tr>
-        <tr>
-          <td>John Johnson</td>
-          <td>Trees</td>
-          <td>500</td>
-          <td>Austim, TX</td>
-          <td>
-            <NavLink className='AllEvents-Actions'>
-              <FaSearchPlus />
-            </NavLink> 
-            <NavLink className='AllEvents-Actions'>
-              <AiFillDelete className='delete' />
-            </NavLink>
-          </td>
-        </tr>
-      </tbody>
-    </Table>  
+
+  //maps over data and inserts it into the body of table
+  const ListOfEvents = EventsData.map((element) =>
+    <tr>
+      <td>{element.speaker}</td>
+      <td>{element.eventName}</td>
+      <td>{element.audienceSize}</td>
+      <td>{element.Location}</td>
+      <td>
+        <NavLink className='AllEvents-Actions'>
+          <FaSearchPlus />
+        </NavLink>
+        <NavLink className='AllEvents-Actions'>
+          <AiFillDelete className='delete' />
+        </NavLink>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div>
+      {/*Creates Table header and then inserts Data from DB into Table body */}
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Speaker</th>
+            <th>Event Name</th>
+            <th>Expected Audience Size</th>
+            <th>Location</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ListOfEvents}
+        </tbody>
+      </Table>
     </div>
-    )
+  )
 }
