fce9b3157f6ebd8f1532509b1dbeecaf6c41c282
pat84571 <pat84571@gmail.com>
2023-02-09 19:23:45 -0700

    emptied the file, no longer needed

--- client/src/Components/EventDetails.js
+++ client/src/Components/EventDetails.js
@@ -1,70 +1,8 @@
 import React, { useState } from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Image,
-  Button,
-  Modal,
-  Form,
-} from "react-bootstrap";
 
 export const EventDetails = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col md={8}>
-          <Image
-            src="https://via.placeholder.com/800x400"
-            fluid
-            className="mb-3"
-          />
-          <h2>Event Name</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            malesuada nisl a varius convallis.
-          </p>
-          <Button variant="primary" onClick={handleShow}>
-            Register
-          </Button>
-        </Col>
-      </Row>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Event Registration</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPhone">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="text" placeholder="Enter phone number" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    </Container>
+    <div></div>
   );
 };
