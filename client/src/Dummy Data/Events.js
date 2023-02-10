a465ab1cc74cf9d704c0beabebc5b03f4d9738cb
pat84571 <pat84571@gmail.com>
2023-02-09 17:04:34 -0700

    dummy data updated

--- client/src/Dummy Data/Events.js
+++ client/src/Dummy Data/Events.js
@@ -1,11 +1,20 @@
-Events = [
+const EventsData = [
     {
+        id:"0",
         speaker: "Mark Otto",
         eventName: "Birds",
         audienceSize: "250",
         Location: "Calgary, AB"
     },
     {
+        id:"1",
+        speaker: "John Johnson",
+        eventName: "Trees",
+        audienceSize: "500",
+        Location: "Austin, TX"
+    },
+    {
+        id:"1",
         speaker: "John Johnson",
         eventName: "Trees",
         audienceSize: "500",
@@ -13,4 +22,4 @@ Events = [
     }
 ]
 
-export default Events;
+export default EventsData;
