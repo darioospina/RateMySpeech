6db195c823d1a4d1989ea7113faf93e3fe35812d
darioospina <65924368+darioospina@users.noreply.github.com>
2023-02-16 14:44:08 -0700

    Report done

--- client/src/Pages/Report.js
+++ client/src/Pages/Report.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { NavBarHeader } from '../Components/Headers/NavBarHeader'
+import { MainReport } from '../Components/MainReport'
 
 export const Report = () => {
+
+
     return (
-        <div className='divContainer'>
-            
-        <p>Report</p>
-        </div>    
+        <MainReport/>
     )
 }
