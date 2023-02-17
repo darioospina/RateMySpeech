49d81d0a00fe9f28a92a444282f93b2b441e94bc
pat84571 <pat84571@gmail.com>
2023-02-16 18:23:39 -0700

    LoginComp is now centered

--- client/src/Components/LoginComp.js
+++ client/src/Components/LoginComp.js
@@ -3,9 +3,9 @@ import Form from 'react-bootstrap/Form';
 
 function LoginComp() {
   return (
-    <Form>
+    <Form id='loginComp'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Email Address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
