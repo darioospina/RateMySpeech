a725c1b5985fa36d9f993026624c21d035b3b223
mohita27 <mohitapunj27@gmail.com>
2023-02-16 22:33:42 -0700

    hi

--- client/src/Components/ProfileDetails.js
+++ client/src/Components/ProfileDetails.js
@@ -19,8 +19,6 @@ export const ProfileDetails = () => {
       <div className="profile-picture-container">
         <img src='../Images/dummy1.jpeg' alt={profile.name} className="profile- picture" />
         <Link to="/profileedit">
-        <img src={profile.picture} alt={profile.name} className="profile- picture" />
-        <Link to="/profileEdit">
     <button className="edit-button">Edit</button>
   </Link>
 </div>
