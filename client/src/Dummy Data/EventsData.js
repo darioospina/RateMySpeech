8492031ba4b81a92599f354d81bca9fbc42c7309
Patryk <pat84571@gmail.com>
2023-03-02 14:52:25 -0700

    renamed fields in prep for DB merge

--- client/src/Dummy Data/EventsData.js
+++ client/src/Dummy Data/EventsData.js
@@ -1,43 +1,39 @@
 const EventsData = [
     {
-        id: "0",
         speaker: "Mark Otto",
-        eventName: "Birds",
-        description:
+        eventname: "Birds",
+        eventdesc:
             "We'll be talking aboit birds! WOW!!!!",
-        auditoriumSize: "250",
-        location: "Calgary, AB",
-        date: "Feb. 25th, 2023"
+        eventcapacity: "250",
+        venue: "Calgary, AB",
+        eventdate: "Feb. 25th, 2023"
     },
     {
-        id: "1",
         speaker: "John Johnson",
-        eventName: "Trees",
-        description:
+        eventname: "Trees",
+        eventdesc:
             "We'll be talking aboit Trees! WOW!!!!",
-        auditoriumSize: "500",
-        location: "Austin, TX",
-        date: "Feb 29th, 2023"
+        eventcapacity: "500",
+        venue: "Austin, TX",
+        eventdate: "Feb 29th, 2023"
     },
     {
-        id: "2",
         speaker: "Michal Smith",
-        eventName: "Plants",
-        description:
+        eventname: "Plants",
+        eventdesc:
             "We'll be talking aboit plants! WOW!!!!",
-        auditoriumSize: "50120",
-        location: "Texas, TX",
-        date: "Mar. 1st, 2023"
+        eventcapacity: "50120",
+        venue: "Texas, TX",
+        eventdate: "Mar. 1st, 2023"
     },
     {
-        id: "3",
         speaker: "Jeremy Poland",
-        eventName: "Space",
-        description:
+        eventname: "Space",
+        eventdesc:
             "We'll be talking aboit space! WOW!!!!",
-        auditoriumSize: "10",
-        location: "RiverDale, AR",
-        date: "Mar. 25th, 2023"
+        eventcapacity: "10",
+        venue: "RiverDale, AR",
+        eventdate: "Mar. 25th, 2023"
     }
 ]
 
