2ae18ff2a22822a800cd204c77e84ca21dd6cf09
darioospina <65924368+darioospina@users.noreply.github.com>
2023-03-05 18:17:48 -0700

    Updated the whole structure of the site

---
+++ client/src/Pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavBarHeader } from '../Components/Headers/NavBarHeader'
+
+export const Profile = () => {
+    return (
+        <div className='divContainer'>
+        <p>Profile</p>
+        <ProfileDetails/>
+        </div>    
+    )
+}
