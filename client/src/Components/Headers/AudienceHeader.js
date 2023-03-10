ab30fd3209795fe8495d00e57cfe300080c4bdd1
Amit Sinha <simranjitsingh2336@gmail.com>
2023-03-10 22:55:28 +0530

    Changes in Header

--- client/src/Components/Headers/AudienceHeader.js
+++ client/src/Components/Headers/AudienceHeader.js
@@ -1,43 +1,65 @@
-import {React,useState, useEffect} from 'react'
-import Logo from '../../Images/logo.png'
-import {useLocation} from 'react-router-dom'
-
+import { React, useState, useEffect } from "react";
+import Logo from "../../Images/logo.png";
+import { useLocation } from "react-router-dom";
 
 //Import Bootstrap components
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 export const AudienceHeader = () => {
-    const [showHeader, setShowHeader] = useState(true);
-    const location = useLocation();
-  
-    useEffect(() => {
-        if (location.pathname === '/questionary') {
-            setShowHeader(true);
-        } else {
-            setShowHeader(false);
-        }
-    }, [location.pathname]);
-  
-    if (!showHeader) return null;
+  const [showHeader, setShowHeader] = useState(true);
+  const location = useLocation();
 
-    return (
-        
-        <div className='divHeader'>
-            <img src={Logo} className="header3-logo" alt="logo"/>
+  useEffect(() => {
+    if (
+      location.pathname === "/questionary" ||
+      location.pathname === "/Questionary"
+    ) {
+      setShowHeader(true);
+    } else {
+      setShowHeader(false);
+    }
+  }, [location.pathname]);
 
-            <Card style={{ width: '90%', margin: '20px auto', backgroundColor: 'white', opacity: 0.7}}>
-            <Card.Body>
-                <ListGroup variant="flush" style={{ backgroundColor: 'white', opacity: 1}}>
-                    <ListGroup.Item><b>Event Name:</b> Latest Developments in Canadian & US Immigration</ListGroup.Item>
-                    <ListGroup.Item><b>Speaker:</b> John Doe</ListGroup.Item>
-                    <ListGroup.Item><b>Venue:</b> Marriott Downtown Calgary</ListGroup.Item>
-                    <ListGroup.Item><b>Event Date:</b> February 22, 2023</ListGroup.Item>
-                </ListGroup>
-            </Card.Body>
-            </Card>
-        </div>
-    )
-}
+  if (!showHeader) return null;
+
+  return (
+    <div className="divHeader">
+      <a href="/Dashboard">
+        <img src={Logo} className="header3-logo" alt="logo" />
+      </a>
+
+      <Card
+        style={{
+          width: "90%",
+          margin: "20px auto",
+          backgroundColor: "white",
+          opacity: 0.7,
+        }}
+      >
+        <Card.Body>
+          <ListGroup
+            variant="flush"
+            style={{ backgroundColor: "white", opacity: 1 }}
+          >
+            <ListGroup.Item>
+              <b>Event Name:</b> Latest Developments in Canadian & US
+              Immigration
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <b>Speaker:</b> John Doe
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <b>Venue:</b> Marriott Downtown Calgary
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <b>Event Date:</b> February 22, 2023
+            </ListGroup.Item>
+          </ListGroup>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
--- client/src/Components/Headers/MainHeader.js
+++ client/src/Components/Headers/MainHeader.js
@@ -1,24 +1,26 @@
-import React, {useState, useEffect} from 'react'
-import Logo from '../../Images/logo.png'
-import {useLocation} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import Logo from "../../Images/logo.png";
+import { useLocation } from "react-router-dom";
 
 export const MainHeader = () => {
-    const [showHeader, setShowHeader] = useState(true);
-    const location = useLocation();
-  
-    useEffect(() => {
-        if (location.pathname === '/' || location.pathname === '/thank-you') {
-            setShowHeader(true);
-        } else {
-            setShowHeader(false);
-        }
-    }, [location.pathname]);
-  
-    if (!showHeader) return null
+  const [showHeader, setShowHeader] = useState(true);
+  const location = useLocation();
 
-    return (
-        <div className='divHeader'>
-            <img src={Logo} className="header1-logo" alt="logo"/>
-        </div>    
-    )
-}
+  useEffect(() => {
+    if (location.pathname === "/" || location.pathname === "/thank-you") {
+      setShowHeader(true);
+    } else {
+      setShowHeader(false);
+    }
+  }, [location.pathname]);
+
+  if (!showHeader) return null;
+
+  return (
+    <div className="divHeader">
+      <a href="/Dashboard">
+        <img src={Logo} className="header1-logo" alt="logo" />
+      </a>
+    </div>
+  );
+};
