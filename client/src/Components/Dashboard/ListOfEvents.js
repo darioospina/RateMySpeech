04483c6d25f3c858d383ccf017c31be7885bc6bc
Patryk <pat84571@gmail.com>
2023-03-16 15:31:30 -0600

    moved loctation of button

--- client/src/Components/Dashboard/ListOfEvents.js
+++ client/src/Components/Dashboard/ListOfEvents.js
@@ -53,7 +53,6 @@ export const ListOfEvents = () => {
   //Maps over all the event data and inserts it into the body of the table
   const ListOfEvents = singleSpeakerData.map((event) =>
     <tr>
-      <td className='eventList-item'>{event.speaker}</td>
       <td className='eventList-item'>{event.eventname}</td>
       <td className='eventList-item'>{event.eventcapacity}</td>
       <td className='eventList-item'>{event.venue}</td>
@@ -76,11 +75,13 @@ export const ListOfEvents = () => {
   return (
     <div id='title-table-block' >
       <h1>All Your Events</h1>
+      <Nav.Link className='list-button' href="/NewEvent">
+        <Button >Create a New Event</Button>
+      </Nav.Link>
       {/*Creates Table header and then inserts Data from DB into Table body */}
       <Table striped>
         <thead>
           <tr>
-            <th>Speaker</th>
             <th>Event Name</th>
             <th>Auditorium Size</th>
             <th>Location</th>
@@ -91,9 +92,6 @@ export const ListOfEvents = () => {
           {ListOfEvents}
         </tbody>
       </Table>
-      <Nav.Link className='list-button' href="/NewEvent">
-        <Button >Create a New Event</Button>
-      </Nav.Link>
 
       {/* Creates the popup that contains specific event details */}
       {singleEventData && (
@@ -102,16 +100,16 @@ export const ListOfEvents = () => {
             <Modal.Title>{singleEventData.eventName}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+            <h2>Event</h2>
+            <p>{singleEventData.eventname}</p>
             <h2>Event Date</h2>
             <p>{singleEventData.eventdate}</p>
-            <h2>Speaker</h2>
-            <p>{singleEventData.speaker}</p>
             <h2>Description</h2>
             <p>{singleEventData.eventdesc}</p>
             <h2>Auditorium Size</h2>
-            <p>{singleEventData.auditoriumSize}</p>
+            <p>{singleEventData.eventcapacity}</p>
             <h2>Location</h2>
-            <p>{singleEventData.location}</p>
+            <p>{singleEventData.venue}</p>
           </Modal.Body>
         </Modal>
       )}
